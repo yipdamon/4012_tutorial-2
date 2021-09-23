2243e51572c596d1eecbd032eb35810956340a3a
yipdamon <yipdamon@hotmail.com>
2021-09-23 19:12:49 -0400

    updating data.memes to 3 from 1

--- scripts/script.js
+++ scripts/script.js
@@ -12,7 +12,7 @@ async function displayMeme(){
         const jsonResponse = await response.json();
         //required
         console.log(jsonResponse);
-        const meme = jsonResponse.data.memes[1];
+        const meme = jsonResponse.data.memes[3];
 
         const image = document.createElement("img");
         image.src = meme.url;
