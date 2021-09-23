4be771a33155fad349963072dbace4c0cbdedf39
yipdamon <yipdamon@hotmail.com>
2021-09-23 18:44:35 -0400

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", initializeApp);
+
+function initializeApp() {
+    displayMeme();
+}
+
+async function displayMeme(){
+    try {
+        const response = await
+        fetch("https://api.imgflip.com/get_memes");
+        const jsonResponse = await response.json();
+        //required
+        console.log(jsonResponse);
+        const meme = jsonResponse.data.memes[1];
+
+        const image = document.createElement("img");
+        image.src = meme.url;
+        image.alt = meme.name;
+        image.width = 200;
+
+        const memeContainer = 
+            document.querySelector("#meme");
+        memeContainer.appendChild(image);
+    } catch (error){
+        console.log("Promise failed", error);
+    }
+}
