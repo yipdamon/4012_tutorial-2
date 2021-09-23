4d85b812c3700a2481caab080d4b026b30a046bd
yipdamon <yipdamon@hotmail.com>
2021-09-23 19:46:49 -0400

    fixed merge

--- scripts/script.js
+++ scripts/script.js
@@ -12,11 +12,7 @@ async function displayMeme(){
         const jsonResponse = await response.json();
         //required
         console.log(jsonResponse);
-<<<<<<< HEAD
-        const meme = jsonResponse.data.memes[2];
-=======
         const meme = jsonResponse.data.memes[3];
->>>>>>> master
 
         const image = document.createElement("img");
         image.src = meme.url;
